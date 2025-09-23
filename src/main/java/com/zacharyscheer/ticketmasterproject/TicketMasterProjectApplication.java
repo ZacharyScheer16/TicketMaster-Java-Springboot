25cc1c5c359622a86552cbb40e9703702c079a9d
Zachary Scheer <zscheer00@gmail.com>
2025-09-22 21:07:20 -0700

    Project Created

---
+++ src/main/java/com/zacharyscheer/ticketmasterproject/TicketMasterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zacharyscheer.ticketmasterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMasterProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketMasterProjectApplication.class, args);
+    }
+
+}
